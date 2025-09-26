aa6a022624070a976699ba4b65c303e8d661829a
tynakovan-art <tyna.kovan@email.cz>
2025-09-26 10:18:41 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import Site from './index'
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Site from "./index"; // pokud máš hlavní komponentu v src/index.tsx
 
-const el = document.getElementById('root')!
-createRoot(el).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Site />
   </React.StrictMode>
-)
+);
