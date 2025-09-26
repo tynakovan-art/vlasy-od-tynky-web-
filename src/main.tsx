9921d924fa70c1572b200c969d7f17170b3ad8d7
tynakovan-art <tyna.kovan@email.cz>
2025-09-26 13:44:52 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-// src/main.tsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Site from "./index";
+import "./index.css"; // DŮLEŽITÉ: sem se natahuje zkompilovaný Tailwind
 
 const el = document.getElementById("root")!;
 createRoot(el).render(
