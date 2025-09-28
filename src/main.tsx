3e8d1f3ed48f274583b65e64532499691b4d617d
tynakovan-art <tyna.kovan@email.cz>
2025-09-29 00:34:57 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Site from "./index";
-import "./index.css";
+import Site from "./index.jsx";
 
-createRoot(document.getElementById("root")!).render(
+const el = document.getElementById("root");
+createRoot(el).render(
   <React.StrictMode>
     <Site />
   </React.StrictMode>
