77e4333eb680232753569f7c6e44ce7d9243a819
tynakovan-art <tyna.kovan@email.cz>
2025-09-29 14:46:30 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Site from "./index.jsx";
-import "./styles.css";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Site />
+    <App />
   </React.StrictMode>
 );
